4e7404a93ffefd5c9c83165e1752b922fbdfaf7c
Brayden Tomlinson <humortronic@gmail.com>
2024-11-04 17:00:42 +1030

    added changes to code

--- ex00/WrongCat.cpp
+++ ex00/WrongCat.cpp
@@ -6,7 +6,24 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:52:38 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/04 16:52:38 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/04 17:00:30 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat()
+{
+    type = "WrongCat";
+    std::cout << "WrongCat created." << std::endl;
+}
+
+WrongCat::~WrongCat()
+{
+    std::cout << "WrongCat destroyed." << std::endl;
+}
+
+void WrongCat::makeSound() const
+{
+    std::cout <<< "Meow? (This is the wrong sound)" << std::endl;
+}
--- ex00/WrongCat.hpp
+++ ex00/WrongCat.hpp
@@ -6,7 +6,22 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:52:41 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/04 16:52:44 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:58:17 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal
+{
+    public:
+        WrongCat();
+        ~WrongCat();
+        
+        void makeSound() const;
+};
+
+#endif
