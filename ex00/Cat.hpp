5b73622a1c19a4446544a002258219c391b5a84f
Brayden Tomlinson <humortronic@gmail.com>
2024-11-04 16:50:47 +1030

    Added changes to code

--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -6,7 +6,22 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:47:04 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/04 16:47:05 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:48:46 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+    public:
+        Cat();
+        ~Cat();
+        
+        void makeSound() const override;
+};
+
+#endif
