71e27f6338b2cf9d6b8a82d24cb2edb35e276fe7
Brayden Tomlinson <humortronic@gmail.com>
2024-11-04 13:32:51 +1030

    Added changes to code

---
+++ ex00/Dog.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 13:31:07 by btomlins          #+#    #+#             */
+/*   Updated: 2024/11/04 13:32:32 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+    public:
+        Dog();
+        ~Dog();
+
+        void makeSound() const override;
+};
+
+#endif
