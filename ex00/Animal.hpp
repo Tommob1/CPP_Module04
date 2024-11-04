7697c19bda67628132ebc4987747487bbe548c80
Brayden Tomlinson <humortronic@gmail.com>
2024-11-04 13:26:39 +1030

    Added changes to code

---
+++ ex00/Animal.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 13:24:26 by btomlins          #+#    #+#             */
+/*   Updated: 2024/11/04 13:26:23 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+    protected:
+        std::string type;
+    
+    public:
+        Animal();
+        virtual ~Animal();
+
+        virtual void makeSound() const;
+        std::string getType() const;
+};
+
+#endif
