f6eb42c8cbfc6769e0b628cd24d3c0db7507bd56
Brayden Tomlinson <humortronic@gmail.com>
2024-11-11 15:45:27 +1030

    Added changes to code

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -6,7 +6,56 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 15:34:38 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/11 15:34:38 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:45:10 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+    int i;
+    i = 0;
+
+    std::cout << "Brain created." << std::endl;
+    while (i < 100)
+    {
+        ideas[i] = "Default idea";
+        ++i;
+    }
+}
+
+Brain::Brain(const Brain &other)
+{
+    int i;
+    i = 0;
+
+    std::cout << "Brain copied." << std::endl;
+    while (i < 100)
+    {
+        ideas[i] = other.ideas[i];
+        ++i;
+    }
+}
+
+Brain &Brain::operator=(const Brain &other)
+{
+    int i;
+    i = 0;
+
+    std::cout << "Brain assigned." << std::endl;
+    if (this != &other)
+    {
+        while (i < 100)
+        {
+            ideas[i] = other.ideas[i];
+            ++i;
+        }
+    }
+    return *this;
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain destroyed." << std::endl;
+}
--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -6,7 +6,25 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 15:34:36 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/11 15:34:39 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:38:04 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+
+class Brain
+{
+    public:
+        std::string ideas[100];
+
+        Brain();
+        Brain(const Brain &other);
+        Brain &operator=(const Brain &other);
+        ~Brain();
+};
+
+#endif
