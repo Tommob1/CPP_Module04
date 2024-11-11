4f1f836361a9492c9ea038b2885d694c78526a2d
Brayden Tomlinson <humortronic@gmail.com>
2024-11-11 16:09:27 +1030

    Added changes to code

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:16:57 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/11 16:06:12 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/11 16:09:04 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,60 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
-int main() {
+int main() 
+{
     std::cout << "--- Correct Polymorphism Example ---" << std::endl;
     const Animal* meta = new Animal();
-    const Animal* dog = new Dog();
-    const Animal* cat = new Cat();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
 
-    std::cout << dog->getType() << " " << std::endl;
-    std::cout << cat->getType() << " " << std::endl;
-    dog->makeSound();
-    cat->makeSound();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    j->makeSound();
+    i->makeSound();
     meta->makeSound();
 
     delete meta;
-    delete dog;
-    delete cat;
+    delete j;
+    delete i;
 
-    std::cout << "\n--- Incorrect Polymorphism Example ---" << std::endl;
-    const WrongAnimal* wrongMeta = new WrongAnimal();
-    const WrongAnimal* wrongCat = new WrongCat();
+    std::cout << "\n--- Array of Animals (Polymorphism) ---" << std::endl;
 
-    std::cout << wrongCat->getType() << " " << std::endl;
-    wrongCat->makeSound();
-    wrongMeta->makeSound();
+    Animal* animal1 = new Dog();
+    Animal* animal2 = new Dog();
+    Animal* animal3 = new Cat();
+    Animal* animal4 = new Cat();
 
-    delete wrongMeta;
-    delete wrongCat;
+    std::cout << animal1->getType() << " says: ";
+    animal1->makeSound();
+
+    std::cout << animal2->getType() << " says: ";
+    animal2->makeSound();
+
+    std::cout << animal3->getType() << " says: ";
+    animal3->makeSound();
+
+    std::cout << animal4->getType() << " says: ";
+    animal4->makeSound();
+
+    delete animal1;
+    delete animal2;
+    delete animal3;
+    delete animal4;
+
+    std::cout << "\n--- Deep Copy Test ---" << std::endl;
+
+    Dog originalDog;
+    originalDog.makeSound();
+
+    Dog copiedDog = originalDog;
+    copiedDog.makeSound();
+
+    Cat originalCat;
+    originalCat.makeSound();
+
+    Cat copiedCat = originalCat;
+    copiedCat.makeSound();
 
     return 0;
 }
